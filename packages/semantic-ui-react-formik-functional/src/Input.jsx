bcf791888117900bd45bd74590f67c64da3a6525
Christian Westgaard <cwe@enonic.com>
2019-07-17 15:30:44 +0200

    Attempt at validation

--- packages/semantic-ui-react-formik-functional/src/Input.jsx
+++ packages/semantic-ui-react-formik-functional/src/Input.jsx
@@ -8,22 +8,31 @@ export const Input = ({
 	parentPath,
 	path = parentPath ? `${parentPath}.${name}` : name,
 	formik: {
+		errors = {},
 		setFieldValue,
+		//validateField,
 		values
 	},
+	validate,
 	onChange = (event, {
 		value: newValue
 	}) => {
 		setFieldValue(path, newValue);
+		validate && validate(newValue);
+		//validateField(path);
 	},
 	value = getIn(values, path, ''),
 	...rest
 }) => {
-	return <SemanticUiReactInput
-		name={path}
-		onChange={onChange}
-		type={type}
-		value={value}
-		{...rest}
-	/>;
+	const error = getIn(errors, path);
+	return <>
+		<SemanticUiReactInput
+			name={path}
+			onChange={onChange}
+			type={type}
+			value={value}
+			{...rest}
+		/>
+		{error && <div>{error}</div>}
+	</>;
 };
