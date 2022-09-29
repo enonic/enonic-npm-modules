24eeaf97347ffccc932b4cfe7188fbc060270215
Mikita Taukachou <edloidas@gmail.com>
2022-09-29 19:49:08 +0300

    Create @enonic/utility-types package #443

---
+++ packages/utility-types/index.d.ts
@@ -0,0 +1,9 @@
+export type Unwrapped<T> = T extends (Array<infer U> | ReadonlyArray<infer U>) ? U : T;
+
+export type Flattened<T> = T extends (Array<infer U> | ReadonlyArray<infer U>) ? Flattened<U> : T;
+
+export type WithRequiredProperty<T, K extends keyof T> = T & { [P in K]-?: T[P] };
+
+export type KeysOfType<O, T> = { [K in keyof O]: O[K] extends T ? K : never }[keyof O];
+
+export type LiteralUnion<T extends U, U = string> = T | (U & Record<never, never>);
