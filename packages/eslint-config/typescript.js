3ed6abe93c543eea5877d6de887c227522f9d405
Mikita Taukachou <edloidas@gmail.com>
2023-07-17 12:13:03 +0200

    Upgrade eslint config peer dependencies #810

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/stylistic',
   ],
   parser: '@typescript-eslint/parser',
   rules: {
