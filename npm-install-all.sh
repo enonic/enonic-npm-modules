9bb2686ea0a89182064ece9fa1000bb58c5ad8d9
alansemenov <ase@enonic.com>
2023-11-01 13:54:36 +0100

    fixed npm install script

--- npm-install-all.sh
+++ npm-install-all.sh
@@ -1,1 +1,12 @@
-find . -name package.json -not -path "*/node_modules/*" -exec bash -c "npm --prefix \$(dirname {}) install --omit=dev --no-audit" \;
+#!/bin/bash
+
+# Find all directories containing package.json files, up to 2 levels deep, excluding node_modules
+directories=$(find . -maxdepth 3 -type d -name "node_modules" -prune -o -type f -name "package.json" -exec dirname {} \;)
+
+# Loop through each directory
+for dir in $directories; do
+    echo "Installing dependencies in $dir"
+    cd "$dir" || exit 1
+    npm install
+    cd - || exit 1
+done
