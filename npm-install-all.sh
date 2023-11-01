dbe9fd94fb743b4c7539039b719be97798b8a90b
alansemenov <ase@enonic.com>
2023-11-01 13:06:27 +0100

    omit dev

--- npm-install-all.sh
+++ npm-install-all.sh
@@ -1,1 +1,1 @@
-find . -name package.json -not -path "*/node_modules/*" -exec bash -c "npm --prefix \$(dirname {}) install --production --no-audit" \;
+find . -name package.json -not -path "*/node_modules/*" -exec bash -c "npm --prefix \$(dirname {}) install --omit=dev --no-audit" \;
